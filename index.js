64e3dba83bfe2975d713832f786e7a264cb8cadc
ladenisko <ladenmail@gmail.com>
2024-06-01 22:00:01 +0200

    Endpoint added, same as 404 status code

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+	if(req.url === '/friends') {
+	// res.writeHead(200, {
+	// 	'Content-Type': 'text/plain',
+	// });
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/json');
+	//res.end('Hello! Sir Isaac Newton is your friend!');
+	res.end(JSON.stringify({
+		id: 1,
+		name: 'Sir Isaac Newton',
+	}));
+} else if (req.url === '/messages') {
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<body>');
+	res.write('<ul>');
+	res.write('<li>Hello Isaac!</li>');
+	res.write('<li>What are you thoughts on astronomy?</li>');
+	res.write('</ul>');
+	res.write('</body>');
+	res.write('</html>');
+	res.end();
+} else {
+	res.statusCode = 404;
+	res.end();
+}
+});
+
+server.listen(PORT, () => {
+	console.log(`listening on port ${PORT}...`);
+});
